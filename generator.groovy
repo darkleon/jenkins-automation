9d1629a9764552678167852e9e73b9a7bb0e2f36
Leurdo, Alexander <Alexander.Leurdo@wolterskluwer.com>
2023-09-19 05:47:25 +0400

    ww

--- generator.groovy
+++ generator.groovy
@@ -1,8 +1,9 @@
-folder("GSSA") {
-  configure { folder ->
-    folder.remove(folder / healthMetrics)
-  }
-}
+// folder("GSSA") {
+//   configure { folder ->
+//     folder.remove(folder / healthMetrics)
+//   }
+// }
 
+folder "GSSA"
 folder "GSSA/a"
 folder "GSSA/a/b"
